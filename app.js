4936beabbd2cc9610db8d13f7b2e961296f9479a
T <toawon@gmail.com>
2016-07-04 15:37:17 +0900

    statics

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 var express = require('express');
-
+var path = require('path');
 var app = express();
 
-app.use(express.static(__dirname + '/public'));
-
+app.use(express.static(path.join(__dirname + '/public')));
+console.log(__dirname);
 app.listen(3000, function(){
   console.log('server on!');
 });
