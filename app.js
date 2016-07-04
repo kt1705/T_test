efaa2f4364adcaa6b56251e4d7080297587cef9f
T <toawon@gmail.com>
2016-07-04 15:47:01 +0900

    no message

--- app.js
+++ app.js
@@ -2,8 +2,13 @@ var express = require('express');
 var path = require('path');
 var app = express();
 
-app.use(express.static(path.join(__dirname + '/public')));
-console.log(__dirname);
+app.set("view engine", 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', funcion(req,res) {
+  res.render('first_ejs');
+});
+
 app.listen(3000, function(){
   console.log('server on!');
 });
